4686c28def97a37dda5240dd5947df120af550b9
Erik Seifert <erik.seifert@b-connect.de>
2015-03-31 17:45:43 +0200

    Update

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/erik.seifert/bc/platforms/android/ant-build/AndroidManifest.xml : \
-/home/erik.seifert/bc/platforms/android/AndroidManifest.xml \
--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/erik.seifert/bc/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/home/erik.seifert/bc/platforms/android/ant-build/CordovaApp.ap_ \
-/home/erik.seifert/bc/platforms/android/ant-build/classes.dex \
-/home/erik.seifert/bc/platforms/android/CordovaLib/ant-build/classes.jar \
